c86c4fa479ef3953479e98599bd9285b1dccddea
Muhammad Rezky Ananda <138985405+mrezkya@users.noreply.github.com>
2023-07-13 17:25:07 +0800

    Add files via upload

--- ok.ino
+++ ok.ino
@@ -4,24 +4,25 @@ LiquidCrystal_I2C lcd(0x27, 16, 12);
 boolean ind = false;
 #define pinSensor A1 //Pin Sensor Water Level
 int nilai = 0; // Menyimpan data sensor di Variable Nilai
+int val = 0;
+//kelompok 2 source code
 void setup() {
 Serial.begin(9600);
 pinMode(7, OUTPUT);
 pinMode(8, OUTPUT);
 pinMode(13, OUTPUT);
-pinMode(3, OUTPUT);
-digitalWrite(13, HIGH);
+//pinMode(3, OUTPUT);
+digitalWrite(13, LOW);
 lcd.begin();
 lcd.backlight();
 lcd.print("aktif");
 }
 
 void loop() {
-int ketinggian = bacaSensor();
+int ketinggian = readSensor();
 Serial.print("Ketinggian Air :" );
 Serial.println(ketinggian);
-delay(200);
-if(ketinggian > 500){
+if(ketinggian > 250){
   lcd.setCursor(1,1);
   lcd.clear();
   digitalWrite(7, HIGH);
@@ -36,9 +37,12 @@ if(ketinggian > 500){
   Serial.println("LOW");
   lcd.print("Mengisi Air");
 }
+delay(2000);
 }
-
-int bacaSensor(){
-nilai = analogRead(pinSensor);
-return nilai;
+int readSensor() {
+  digitalWrite(13, HIGH);  
+  delay(10);              
+  val = analogRead(pinSensor);
+  digitalWrite(13, LOW);   
+  return val;            
 }
